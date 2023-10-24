4096b7b4ff150a3b60ee0f5186205d8446a530a0
Eduard <110898623+eduardtomas1@users.noreply.github.com>
2023-10-24 16:54:12 +0200

    estructura projecte

---
+++ projecteMP12/WikiLocMila/src/main/java/com/mycompany/wikilocmila/WikiLocMila.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wikilocmila;
+
+/**
+ *
+ * @author eduar
+ */
+public class WikiLocMila {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ projecteMP12/WikiLocMilaModel/src/main/java/com/mycompany/wikilocmilamodel/WikiLocMilaModel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wikilocmilamodel;
+
+/**
+ *
+ * @author eduar
+ */
+public class WikiLocMilaModel {
+
+    public static void main(String[] args) {
+        
+    }
+}
